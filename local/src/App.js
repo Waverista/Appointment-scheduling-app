6b25ea8f1e33a252ce505a594a96015a4d456316
Ishan Tharindu <t.gunawardana864@gmail.com>
2023-08-21 14:50:30 +0530

    Update App.js

--- local/src/App.js
+++ local/src/App.js
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import Router from "./router";
+import Router from "./router/index.js";
 
 function App() {
   return (
