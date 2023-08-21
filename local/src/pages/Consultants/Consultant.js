2dfbea91d20be34668c1aeb5fe821a3db48bc0a7
Ishan Tharindu <t.gunawardana864@gmail.com>
2023-08-22 00:55:44 +0530

    integrate with get consultants endpoint

--- local/src/pages/Consultants/Consultant.js
+++ local/src/pages/Consultants/Consultant.js
@@ -3,8 +3,10 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Modal from "react-modal";
+import Swal from "sweetalert2";
+import { ProviderContext } from "../../components/Provider/Provider";
 
 export default function Consultant() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -16,69 +18,85 @@ export default function Consultant() {
   const closeModal = () => {
     setIsModalOpen(false);
   };
+
+  const { axiosJWT, consultantsLst, setConsultantsLst } =
+    useContext(ProviderContext);
+
+  const getConsultants = async () => {
+    try {
+      const res = await axiosJWT.get("http://localhost:4000/consultants", {
+        headers: {
+          authorization: "Bearer " + sessionStorage.getItem("accessToken"),
+        },
+      });
+      setConsultantsLst(res.data);
+    } catch (err) {
+      console.log(err);
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        text: "Server Error.",
+        showConfirmButton: false,
+        timer: 5000,
+      });
+    }
+  };
+  useEffect(() => {
+    getConsultants();
+  }, []);
+
+  const [userType] = useState(sessionStorage.getItem("userType"));
+
   return (
     <>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image="https://www.seekpng.com/png/detail/131-1314988_icon-consulting-anonymous-proxy-icon.png"
-            alt="IT consultant"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              John John
-            </Typography>
-            <Typography variant="body4" color="text.secondary">
-              IT consultant
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="success" onClick={openModal}>
-            View
-          </Button>
-        </CardActions>
-        <Modal
-          isOpen={isModalOpen}
-          onRequestClose={closeModal}
-          style={{
-            content: {
-              width: "800px",
-              height: "500px",
-              margin: "auto",
-            },
-          }}
-        >
-          <h2>Modal Content</h2>
-          <p>This is the content of the modal.</p>
-          <button onClick={closeModal}>Close</button>
-        </Modal>
-      </Card>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image="https://www.seekpng.com/png/detail/131-1314988_icon-consulting-anonymous-proxy-icon.png"
-            alt="IT consultant"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              John John
-            </Typography>
-            <Typography variant="body4" color="text.secondary">
-              IT consultant
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="success">
-            View
-          </Button>
-        </CardActions>
-      </Card>
+      {consultantsLst.map((consultant) => (
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image="https://www.seekpng.com/png/detail/131-1314988_icon-consulting-anonymous-proxy-icon.png"
+              alt="IT consultant"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {consultant.name}
+              </Typography>
+              <Typography variant="body4" color="text.secondary">
+                Email: {consultant.email}
+              </Typography>
+              <br />
+              <Typography variant="body4" color="text.secondary">
+                Country: {consultant.country}
+              </Typography>
+              <br />
+              <Typography variant="body4" color="text.secondary">
+                Job Type: {consultant.job_type}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="success" onClick={openModal}>
+              View
+            </Button>
+          </CardActions>
+          <Modal
+            isOpen={isModalOpen}
+            onRequestClose={closeModal}
+            style={{
+              content: {
+                width: "800px",
+                height: "500px",
+                margin: "auto",
+              },
+            }}
+          >
+            <h2>Modal Content</h2>
+            <p>This is the content of the modal.</p>
+            <button onClick={closeModal}>Close</button>
+          </Modal>
+        </Card>
+      ))}
     </>
   );
 }
